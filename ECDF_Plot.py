007612c90b6c48fa2de2e5da211da7cddbd08782
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2018-07-18 00:55:22 -0700

    Update ECDF_Plot.py

--- ECDF_Plot.py
+++ ECDF_Plot.py
@@ -1,19 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 def ecdf(data, xlab, ylab):
+    
     """
     Args - data: array
            xlab: Label for x axis
            ylab: Label for y axis
     Function: Plots ECDF
     """
+    
     # Number of data points
     nums = len(data)
-
     # x axis data
     x = np.sort(data)
-
     # y axis data
-    y = np.arange(1, n+1) / n
+    y = np.arange(1, nums + 1) / nums
     
+    # Plot ecdf
     plt.plot(x, y, marker='.', linestyle='none')
     plt.xlabel(xlab)
     plt.ylabel(ylab)
